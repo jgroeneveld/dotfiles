a5c307dda39de0da25db332e769bfc7a88f272d7
Jaap Groeneveld <jaapgr@gmail.com>
2016-12-12 13:51:08 +0100

    some changes

--- other_files/git-trim-whitespace.sh
+++ other_files/git-trim-whitespace.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-# removes whitespaces from lines containing only whitespaces on staged files
-
-git diff --cached --name-only | xargs sed -i.bak -E -e 's/^ *$//' 
-git diff --cached --name-only | xargs -I{} rm {}.bak
+# removes whitespaces from lines containing only whitespaces on staged files 
+# that are new or modified (not deleted)
+git diff --cached --name-status | grep "^M\|^A" | cut -f 2 | xargs sed -i.bak -E -e 's/^ *$//' 
+git diff --cached --name-status | grep "^M\|^A" | cut -f 2 | xargs -I{} rm {}.bak
